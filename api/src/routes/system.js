6614f687415a770d3c47ef8e72f792beb6f5a920
Dylan Schirino <dylan.schirino@hotmail.fr>
2017-02-12 15:02:36 +0100

    Adding error controllers system

--- api/src/routes/system.js
+++ api/src/routes/system.js
@@ -7,9 +7,11 @@
 import { Router } from "express";
 
 import sysEchoController from "../controllers/system/echo";
+import sysErrorController from "../controllers/system/error";
 
 let oRouter = new Router();
 
 oRouter.get( "/sys/echo", sysEchoController );
+oRouter.get( "/sys/error", sysErrorController );
 
 export default oRouter;
