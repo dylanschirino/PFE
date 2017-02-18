33ce4fcca994931c239506e8ec52b5edb9adb595
Dylan Schirino <dylan.schirino@hotmail.fr>
2017-02-18 12:55:13 +0100

    Adding core express file

---
+++ www/build/src/core/express.js
@@ -0,0 +1,50 @@
+/* Dylan/PFE/Api
+*
+* /src/core/express.js Express JS file
+*
+* At 18/02/17
+*/
+import express from "express";
+import bodyParser from "body-parser";
+import responseTime from "response-time";
+import mitanEko from "mitan-eko";
+import zouti from "zouti";
+//import systemRoutes from "../routes/system";
+
+let oApp,
+    fInit;
+const APP_PORT = 8080;
+
+fInit = function( iAppPort = APP_PORT ) {
+    if ( oApp ) {
+        return oApp;
+    }
+    oApp = express();
+
+    // Configure middlewares( small softwares components )
+
+    oApp.use( mitanEko( "api" ) );
+    oApp.use( responseTime() );
+    oApp.use( bodyParser.json() );
+    oApp.use( bodyParser.urlencoded( {
+        "extended": true,
+    } ) );
+
+    oApp.use( express.static( `${ __dirname }/../../static` ) );
+
+    // configure templates
+    oApp.set( "views", `${ __dirname }/../views` );
+    oApp.set( "view engine", "pug" );
+
+    // Routes
+    //oApp.use( systemRoutes );
+
+    // Listening on port
+    oApp.listen( iAppPort, () => {
+        zouti.success( `Server is listening on the port ${ iAppPort }.`, "API" );
+    } );
+};
+
+export {
+  fInit as init,
+};
