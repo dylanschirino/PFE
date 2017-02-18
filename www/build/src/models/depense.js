ba19d2ec7fff1b20f745e5444e610a34917660b1
Dylan Schirino <dylan.schirino@hotmail.fr>
2017-02-18 14:05:29 +0100

    Adding depense models

---
+++ www/build/src/models/depense.js
@@ -0,0 +1,44 @@
+/* Dylan/PFE/Api
+*
+* /src/models/depense.js Depense MODELS JS file
+*
+* At 18/02/17
+*/
+import { db } from "../core/mongodb";
+import { ObjectID } from "mongodb";
+import Promise from "bluebird";
+
+let fCheckDepense;
+
+// On vérifie qu'on a bien un ID dans les dépenses
+fCheckDepense = function( sDepenseID ) {
+    let oDepenseID;
+
+    // Si il n'y a pas d'id
+    if ( sDepenseID ) {
+        return Promise.resolve( false );
+    }
+    try {
+        oDepenseID = new ObjectID( sDepenseID );
+    } catch ( oError ) {
+        return Promise.reject( new Error( "Invalid Depense ID" ) );
+    }
+
+    return db.collection( "depense" )
+      .findOne( {
+          "_id": oDepenseID,
+      } )
+      .then( ( oDepenseID ) => {
+          if ( oDepenseID ) {
+              return Promise.resolve( true );
+          }
+          return Promise.reject( new Error( "Unknow depense" ) );
+      } );
+};
+
+export default function() {
+    return db.collection( "depense" );
+}
+export {
+  fCheckDepense as checkDepense,
+};
