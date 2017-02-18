0cb2f4998544cd42304cad03e3b171fcadcc7569
Dylan Schirino <dylan.schirino@hotmail.fr>
2017-02-18 17:33:14 +0100

    Adding destroy route

--- www/build/src/routes/depense.js
+++ www/build/src/routes/depense.js
@@ -8,6 +8,7 @@ import { Router } from "express";
 
 import create from "../controllers/depense/create";
 import update from "../controllers/depense/update";
+import destroy from "../controllers/depense/destroy";
 import list from "../controllers/depense/list";
 import details from "../controllers/depense/details";
 
@@ -17,5 +18,6 @@ oRouter.get( "/depense", list );
 oRouter.get( "/depense/:id", details );
 oRouter.post( "/depense", create );
 oRouter.patch( "/depense/:id", update );
+oRouter.delete( "/depense/:id", destroy );
 
 export default oRouter;
