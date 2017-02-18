4e4aaf49a26503ac51070800e1489c61edd839d3
Dylan Schirino <dylan.schirino@hotmail.fr>
2017-02-18 12:38:50 +0100

    Adding mongodb core

---
+++ www/build/src/core/mongodb.js
@@ -0,0 +1,33 @@
+/* Dylan/PFE/Api
+*
+* /src/core/mongodb.js MongoDB JS file
+*
+* At 18/02/17
+*/
+import { MongoClient } from "mongodb";
+import Promise from "bluebird";
+import zouti from "zouti";
+
+const MONGO_URL = "mongodb://127.0.0.1/api";
+
+let oDB, fInit;
+
+// Fonction d'initialisation de la base de données
+fInit = function() {
+    return new Promise( ( fResolve, fReject ) => {
+        MongoClient.connect( MONGO_URL, ( oError, oLinkedDB ) => {
+
+            if ( oError ) {
+                return fReject( oError );
+            }
+            // Si y a pas d'erreur on affiche le message de connection
+            zouti.success( "Connected to DB", "API" );
+            fResolve( oDB = oLinkedDB );
+        } );
+    } );
+};
+
+export {
+  fInit as init,
+  oDB as db,
+};
