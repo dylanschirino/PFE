51cb33ac178d3afb8930ac7e9e5f9ec84a3ec050
Dylan Schirino <dylan.schirino@hotmail.fr>
2017-02-23 18:14:50 +0100

    Adding pret details controllers

---
+++ www/build/src/controllers/pret/details.js
@@ -0,0 +1,46 @@
+/* Dylan/PFE/Api
+*
+* /src/controllers/pret/details.js Pret details controllers
+*
+* At 23/02/17
+*/
+import getPret from "../../models/pret";
+import { send, error } from "../../core/utils/api";
+import { ObjectID } from "mongodb";
+
+export default function( oRequest, oResponse ) {
+    let sPretID = ( oRequest.params.id || "" ).trim();
+
+    if ( !sPretID ) {
+        error( oRequest, oResponse, "Invalid pret ID", 400 );
+    }
+
+    getPret()
+    .findOne( {
+        "_id": new ObjectID( sPretID ),
+        "deleted_at": null,
+    } )
+    .then( ( oPret ) => {
+        if ( !oPret ) {
+            return error( oRequest, oResponse, "Unknow Pret", 404 );
+        }
+
+        let { _id, name, montant, interet, mensualite, duree, depart, created_at } = oPret,
+            oCleanPret;
+
+        oCleanPret = {
+            "id": _id,
+            "name": name,
+            "montant": montant,
+            "interet": interet,
+            "mensualite": mensualite,
+            "duree": duree,
+            "depart": depart,
+            "created_at": created_at,
+        };
+        send( oRequest, oResponse, oCleanPret );
+    } )
+    .catch( ( oError ) => {
+        error( oRequest, oResponse, oError );
+    } );
+}
--- www/build/src/routes/pret.js
+++ www/build/src/routes/pret.js
@@ -10,7 +10,7 @@ import create from "../controllers/pret/create";
 import update from "../controllers/pret/update";
 import destroy from "../controllers/pret/destroy";
 import list from "../controllers/pret/list";
-// import details from "../controllers/pret/details";
+import details from "../controllers/pret/details";
 
 let oRouter = new Router();
 
@@ -18,6 +18,6 @@ oRouter.post( "/pret", create );
 oRouter.patch( "/pret/:id", update );
 oRouter.delete( "/pret/:id", destroy );
 oRouter.get( "/pret/", list );
-// oRouter.get( "/pret/:id", details );
+oRouter.get( "/pret/:id", details );
 
 export default oRouter;
