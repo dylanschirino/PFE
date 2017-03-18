e3ac1cd7241068e5c783163519f30812260b0f8f
Dylan Schirino <dylan.schirino@hotmail.fr>
2017-03-18 18:38:24 +0100

    Adding subscription page style and react-code

--- InTheWallet/style/SubscribeStyle.js
+++ InTheWallet/style/SubscribeStyle.js
@@ -1,18 +1,71 @@
 import EStyleSheet from 'react-native-extended-stylesheet';
 
 EStyleSheet.build({
-  $titleColor: '#235182'
+  $titleColor: '#235182',
+  $bodyColor:'#F8FDFF',
+  $input:'#538EB6',
 });
 
 module.exports = EStyleSheet.create({
+  body:{
+    backgroundColor:'$bodyColor',
+    flex:1,
+  },
+  titleContainer:{
+    alignSelf:'center',
+    borderBottomWidth:1,
+    borderColor:'$titleColor',
+    marginBottom:75,
+  },
   title:{
     fontFamily:'lato-light',
     fontSize:32,
     textAlign:'center',
     marginTop:50,
-    color: '$titleColor'
+    color: '$titleColor',
+    paddingBottom:5,
   },
-  svg:{
-    flex:1,
+  inputContainer:{
+    width:'83%',
+    alignSelf:'center',
+  },
+  icon:{
+    width:30,
+    height:25,
+    marginRight:5,
+  },
+  label:{
+    flexWrap:'nowrap',
+    fontSize:12,
+    lineHeight:30,
+    color:'$titleColor',
+  },
+  inputBox:{
+    borderBottomWidth:1,
+    borderColor:'$input',
+    marginBottom:25,
+  },
+  input:{
+    height:30,
+    color:'$input',
+  },
+  button:{
+    paddingTop:19,
+    paddingBottom:19,
+    borderWidth:1,
+    borderColor:'$input',
+    borderRadius:8,
+    alignItems:'center',
+    alignSelf:'stretch',
+    marginBottom:80,
+  },
+  buttonText:{
+    color:'$input',
+    fontFamily:'lato-regular',
+    fontSize:16,
+  },
+  cercle:{
+    width:'100%',
+    height:100,
   }
 });
