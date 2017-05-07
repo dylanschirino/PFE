00db1aae37f8425301c0083775866c82097b1941
Dylan Schirino <dylan.schirino@hotmail.fr>
2017-05-07 17:26:54 +0200

    Reload the page when an epargne is deleted

--- InTheWallet/app/components/epargne/epargne.js
+++ InTheWallet/app/components/epargne/epargne.js
@@ -104,6 +104,15 @@ let Epargne = React.createClass ({
     .catch(function (error) {
       alert('Erreur:'+ error);
     });
+    axios.get('http://104.131.74.22:8080/epargne?user='+this.props.username)
+    .then( response => {
+      const epargneObject = response.data['data'];
+      const epargneArray = Object.keys(epargneObject).map(key => epargneObject[key]);
+      this.setState({ epargneArray });
+    })
+    .catch(function (error) {
+      alert('Erreur:'+ error);
+    });
   },
   goDetails(id,name,end){
     this.props.navigator.push({
