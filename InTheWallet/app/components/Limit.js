0411afd7296b268bed62aad81b3def10eb3129c8
Dylan Schirino <dylan.schirino@hotmail.fr>
2017-05-11 12:06:08 +0200

    Refractor code for limit

--- InTheWallet/app/components/Limit.js
+++ InTheWallet/app/components/Limit.js
@@ -92,93 +92,118 @@ let Limit = React.createClass ({
         });
       }
     }
-},
+  },
+  _renderHeader(){
+    return(
+      <View style={nav.header}>
+        <StatusBar barStyle="light-content"
+          />
+        <View style={nav.navBar}>
+          <TouchableOpacity style={nav.backLink} onPress={this.goHome}>
+            <Image style={nav.backIcone} source={ require('../img/back.png')}
+              />
+            <Text style={nav.backText}>Accueil</Text>
+          </TouchableOpacity>
+          <Text style={nav.navTitleCustomLimit}>Limite du mois</Text>
+        </View>
+      </View>
+    )
+  },
+  _renderInfo(){
+    return(
+      <View style={limit.content}>
+        <View style={limit.contentHead}>
+          <Image style={limit.iconTitle} source={ require('../img/question.png')}
+            />
+          <Text style={limit.title}>{`Qu'est-ce que la limite du mois ?`.toUpperCase()}</Text>
+        </View>
+        <View style={limit.contentInfo}>
+          <Text style={limit.text}>La limite du mois est un montant  que vous vous fixez comme objectif à ne pas dépasser.</Text>
+          <Text style={limit.text}>Il est important de vous fixer une limite qui vous corresponds, ne soyez pas trop audacieux tout en essayant de vous mettre au défi.</Text>
+        </View>
+      </View>
+    )
+  },
+  _renderForm(){
+    return(
+      <View style={limit.formContainer}>
+        <Text style={limit.titleForm}>{'Limite du mois'.toUpperCase()}</Text>
+        <Form style={limit.formContent} ref="limite">
+          <View style={limit.iconContainer}>
+            <Image style={limit.iconForm} source={ require('../img/limit.png')}
+              />
+          </View>
+          <View style={limit.input}>
+            <TextInput style={limit.inputText}
+              ref="limit"
+              onChangeText={(text) => {
+                this.setState( {limit:text} );
+              }}
+              value={this.state.limit.toString()}
+              color='#333333'
+              placeholderTextColor='#ACACAC'/>
+            <Text style={limit.euro}>€</Text>
+          </View>
+        </Form>
+      </View>
+    )
+  },
+  _renderAction(){
+    return(
+      <View style={limit.actionContainer}>
+        <TouchableOpacity style={limit.cancelContainer} onPress={this.cancelHome}>
+          <Image source={require('../img/cancel.png')} style={limit.cancelIcon}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity style={limit.checkContainer} onPress={this._handlePress}>
+          <Image source={require('../img/check.png')} style={limit.checkIcon}
+          />
+        </TouchableOpacity>
+      </View>
+    )
+  },
+  _renderMenu(){
+    return(
+      <View style={menu.menu}>
+          <TouchableOpacity style={menu.menuLink}>
+            <Image
+              style={menu.icone}
+              source={ require('../img/home.png')}
+            />
+          <Text style={menu.menuLabel}>Accueil</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={menu.menuLink} onPress={this.goDepense}>
+            <Image
+              style={menu.icone}
+              source={ require('../img/depense.png')}
+            />
+          <Text style={menu.menuLabel}>Dépenses</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={menu.menuLink} onPress={this.goEpargne}>
+            <Image
+              style={menu.iconeEpargne}
+              source={ require('../img/epargne.png')}
+            />
+          <Text style={menu.menuLabel}>Épargne</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={menu.menuLink} onPress={this.goPret}>
+            <Image
+              style={menu.iconePret}
+              source={ require('../img/pret.png')}
+            />
+          <Text style={menu.menuLabel}>Prêt</Text>
+          </TouchableOpacity>
+      </View>
+    )
+  },
   render() {
     return (
       <View style={{flex:1,}}>
-        <View style={nav.header}>
-          <StatusBar barStyle="light-content"
-            />
-          <View style={nav.navBar}>
-            <TouchableOpacity style={nav.backLink} onPress={this.goHome}>
-              <Image style={nav.backIcone} source={ require('../img/back.png')}
-                />
-              <Text style={nav.backText}>Accueil</Text>
-            </TouchableOpacity>
-            <Text style={nav.navTitleCustomLimit}>Limite du mois</Text>
-          </View>
-        </View>
-        <View style={limit.content}>
-          <View style={limit.contentHead}>
-            <Image style={limit.iconTitle} source={ require('../img/question.png')}
-              />
-            <Text style={limit.title}>{`Qu'est-ce que la limite du mois ?`.toUpperCase()}</Text>
-          </View>
-          <View style={limit.contentInfo}>
-            <Text style={limit.text}>La limite du mois est un montant  que vous vous fixez comme objectif à ne pas dépasser.</Text>
-            <Text style={limit.text}>Il est important de vous fixer une limite qui vous corresponds, ne soyez pas trop audacieux tout en essayant de vous mettre au défi.</Text>
-          </View>
-        </View>
-        <View style={limit.formContainer}>
-          <Text style={limit.titleForm}>{'Limite du mois'.toUpperCase()}</Text>
-          <Form style={limit.formContent} ref="limite">
-            <View style={limit.iconContainer}>
-              <Image style={limit.iconForm} source={ require('../img/limit.png')}
-                />
-            </View>
-            <View style={limit.input}>
-              <TextInput style={limit.inputText}
-                ref="limit"
-                onChangeText={(text) => {
-                  this.setState( {limit:text} );
-                }}
-                value={this.state.limit.toString()}
-                color='#333333'
-                placeholderTextColor='#ACACAC'/>
-              <Text style={limit.euro}>€</Text>
-            </View>
-          </Form>
-        </View>
-        <View style={limit.actionContainer}>
-          <TouchableOpacity style={limit.cancelContainer} onPress={this.cancelHome}>
-            <Image source={require('../img/cancel.png')} style={limit.cancelIcon}
-            />
-          </TouchableOpacity>
-          <TouchableOpacity style={limit.checkContainer} onPress={this._handlePress}>
-            <Image source={require('../img/check.png')} style={limit.checkIcon}
-            />
-          </TouchableOpacity>
-        </View>
-        <View style={menu.menu}>
-            <TouchableOpacity style={menu.menuLink}>
-              <Image
-                style={menu.icone}
-                source={ require('../img/home.png')}
-              />
-            <Text style={menu.menuLabel}>Accueil</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={menu.menuLink} onPress={this.goDepense}>
-              <Image
-                style={menu.icone}
-                source={ require('../img/depense.png')}
-              />
-            <Text style={menu.menuLabel}>Dépenses</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={menu.menuLink} onPress={this.goEpargne}>
-              <Image
-                style={menu.iconeEpargne}
-                source={ require('../img/epargne.png')}
-              />
-            <Text style={menu.menuLabel}>Épargne</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={menu.menuLink} onPress={this.goPret}>
-              <Image
-                style={menu.iconePret}
-                source={ require('../img/pret.png')}
-              />
-            <Text style={menu.menuLabel}>Prêt</Text>
-            </TouchableOpacity>
-        </View>
+        {this._renderHeader()}
+        {this._renderInfo()}
+        {this._renderForm()}
+        {this._renderAction()}
+        {this._renderMenu()}
       </View>
     )}
   });
