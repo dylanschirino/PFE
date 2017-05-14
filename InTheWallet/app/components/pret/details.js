300355bf88800862cf9cdd3fd9190bfb9f312986
Dylan Schirino <dylan.schirino@hotmail.fr>
2017-05-14 14:50:29 +0200

    Fix bug with timer too early or late

--- InTheWallet/app/components/pret/details.js
+++ InTheWallet/app/components/pret/details.js
@@ -44,7 +44,7 @@ let Details = React.createClass ({
       var timeStampFinal = new Date(newDate).getTime();
       var distance = (timeStampFinal - timeStamp);
       var month = Math.floor( distance / (1000 * 60 * 60 * 24 * 31));
-      var days = Math.floor((distance / (1000 * 60 * 60 * 24)));
+      var days = Math.floor((distance / (1000 * 60 * 60 * 24))+1);
       if( days == 0 ){
         var timeS = 'PRÊT CLOTURÉ'
       }
